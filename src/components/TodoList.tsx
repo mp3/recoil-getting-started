fb187d8c6e1f93a5363e67322aaa67f5129d6a9a
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-01 22:43:11 +0900

    todoItem

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,123 @@
+import React, { useState, ChangeEvent } from 'react'
+import {
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState
+} from 'recoil'
+
+type TodoItem = {
+  id: number
+  text: string
+  isCompleted: boolean
+}
+
+const todoListState = atom<TodoItem[]>({
+  key: 'todoListState',
+  default: []
+})
+
+export const TodoList = () => {
+  const todoList = useRecoilValue(todoListState)
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListFilters /> */}
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  )
+}
+
+const TodoItemCreator = () => {
+  const [inputValue, setInputValue] = useState('')
+  const setTodoList = useSetRecoilState(todoListState)
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isCompleted: false
+      }
+    ])
+    setInputValue('')
+  }
+
+  const onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(value)
+  }
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  )
+}
+
+let id = 0
+const getId = () => {
+  return id++
+}
+
+const TodoItem = ({ item }: { item: TodoItem }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState)
+  const index = todoList.findIndex((listItem) => listItem === item)
+
+  const editItemText = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: value
+    })
+
+    setTodoList(newList)
+  }
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isCompleted: !item.isCompleted
+    })
+
+    setTodoList(newList)
+  }
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index)
+
+    setTodoList(newList)
+  }
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isCompleted}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  )
+}
+
+const replaceItemAtIndex = (arr: any[], index: number, newValue: any) => {
+  return [...arr].map((item, i) => {
+    if (index === i) {
+      return newValue
+    }
+
+    return item
+  })
+}
+
+const removeItemAtIndex = (arr: any[], index: number) => {
+  return [...arr].filter((_item, i) => index !== i)
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { RecoilRoot } from 'recoil'
 import { CharacterCounter } from './components/CharacterCounter'
+import { TodoList } from './components/TodoList'
 
 const Main = () => {
   return (
     <RecoilRoot>
       <CharacterCounter />
+      <TodoList />
     </RecoilRoot>
   )
 }
